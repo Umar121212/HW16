034d9e1466a8f68491d63e5bd7a1169db0e0a4b3
Umar121212 <iskandarov.u12@gmail.com>
2023-09-25 01:37:46 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+    // Домашка 17
+
+    function calcSquare (a, b) {
+        // return console.log((a + b)* 2)
+        
+    }
+    
+
+    function square (a) {
+        return (a * a)
+         
+    };
+    // console.log(square())
+
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  for (i = 0; i < arr.length; i++ ) {
+    // console.log(arr[i] * 2)
+  }
+
+  
